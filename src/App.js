74e557ca6100461f8a66c857b711e2be67ac21b6
JohanaDahan <74196888+JohanaDahan@users.noreply.github.com>
2021-12-11 16:27:24 +0100

    fini

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import InstantHeartrate from "./Composant/InstantHeartrate";
+import Graphic from "./Composant/Graphic"
+import { useEffect, useState } from "react";
 
 
-
-function RH(){
+function randomHeartrate(){
   return Math.floor(Math.random()*160) + 40;
 }
+
 function App() {
+  //Initialise les propriété
+  //Retourne une propriétés et une fonction pour la modifier
+  const [measurements, setMeasurement] = useState([]);
+
+  // Fonction qui est appelé lors du chargement de la page
+  // Défini un interval de 2 secondes
+  // Modifie la valeur de la propriété toutes les 2 secondes
+  // Conserve le tableau précédent, en crée un nouveau et ajoute la valeur
+  useEffect(() => {
+    setInterval(() => {
+      setMeasurement((prev) => {
+        const measurement = {
+          timestamp: new Date().getTime(),
+          heartrate: randomHeartrate(),
+        }
+        return [...prev, measurement]
+      });
+    }, 2000);
+  }, [])
+
+  //Affiche seulement la dernière valeur du tableau
+  const measurement = measurements[measurements.length -1];
+  console.log(measurements)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       <RH/> BPM
+          Modifier le fichier <code>src/App.js</code> et le rechargement sera automatique.
         </p>
+        <InstantHeartrate data={measurement}/>
+        <Graphic datalist={measurements}/>
       </header>
     </div>
   );
 }
 
-
 export default App;
-
---
+++ src/Composant/InstantHeartrate.js
@@ -0,0 +1,19 @@
+function InstantHeartrate(props){
+    const {data} = props
+    let couleur = "rythmeBas"
+  
+    if (!data){return(<p>Pas de valeurs</p>)}
+  
+    if(data.heartrate > 60 && data.heartrate < 160){
+      couleur = "rythmeNormal"
+    }
+    if(data.heartrate > 160){
+      couleur = "rythmeEleve"
+    }
+  
+    return(
+      <div className={couleur}>Rythme cardiaque : {data.heartrate}</div>
+    )
+  }
+  
+  export default InstantHeartrate;
